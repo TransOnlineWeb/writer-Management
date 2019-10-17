a76eba4d8b3978a5ac730cfdfd8170562cb8742e
Ochieng424 <ochiengderrick424@gmail.com>
2019-10-17 10:44:23 +0300

    swal

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,18 @@ const router = new VueRouter({
     routes // short for `routes: routes`
 });
 
+//sweetalert
+import swal from 'sweetalert2';
+window.swal = swal;
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+window.toast = toast;
+window.Fire =  new Vue();
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
