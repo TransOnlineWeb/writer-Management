a0b5abdcb8601868aabe8a2af7abc7a4a0546684
jeremihz <mihjere68@gmail.com>
2019-10-21 13:32:16 +0300

    dashboard

--- public/js/app.js
+++ public/js/app.js
@@ -83591,8 +83591,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/Transonline/writer-Management/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Transonline/writer-Management/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\mihz\Desktop\writer-Management\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\mihz\Desktop\writer-Management\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
