af8e04d77b75f10ad0e2b78fba39dbf3f2b69612
Ochieng424 <ochiengderrick424@gmail.com>
2019-11-05 13:21:38 +0300

    writer category add complete

---
+++ app/Http/Controllers/API/WriterCategoryController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WriterCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::latest()->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'title' => 'required|string|max:125|unique:categories',
+            'amount' => 'required'
+        ]);
+
+        $category = new Category();
+        $category->title = $request->title;
+        $category->amount = $request->amount;
+        $category->save();
+
+        return response(['status' => 'success'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
