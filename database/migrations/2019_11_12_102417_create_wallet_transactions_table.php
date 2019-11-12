2af359244b00961c77a81cff801e8056e37e248c
Ochieng424 <ochiengderrick424@gmail.com>
2019-11-12 10:37:25 +0300

    working on verifying completed task

---
+++ database/migrations/2019_11_12_102417_create_wallet_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWalletTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallet_transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('type');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('order_number');
+            $table->decimal('amount', 12, 2);
+            $table->decimal('percentage')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallet_transactions');
+    }
+}
