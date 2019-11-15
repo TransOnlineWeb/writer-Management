f05a45631d2c0b3d32a67a994d06d9239ff2eb61
jeremihz <mihjere68@gmail.com>
2019-11-15 17:59:47 +0300

    content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,8 +128,9 @@
                 <div class="container">
                     <div class="row animate-box">
                         <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-                            <h2>Join And Be An Expert</h2>
-                            <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+                            <h2>Your Writting Talent Pays off</h2>
+                            
+                            <p>We give you access to thousand of projects.Be part of us and turn your writting skills into cash</p>
                         </div>
                     </div>
                     <div class="row animate-box">
